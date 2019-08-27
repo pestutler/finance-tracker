b7e8f5b4a6b17207616ee3f69bf03dece2d77046
Paul Stutler <pstutler@gmail.com>
2019-08-27 11:33:07 -0500

    Added friendships capability and finished application

--- db/migrate/20190822221539_create_friendships.rb
+++ db/migrate/20190822221539_create_friendships.rb
@@ -1,7 +1,7 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.belongs_to :users
+      t.belongs_to :user
       t.belongs_to :friend, class: 'User'
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 ActiveRecord::Schema.define(version: 2019_08_22_221539) do
 
   create_table "friendships", force: :cascade do |t|
-    t.integer "users_id"
+    t.integer "user_id"
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["users_id"], name: "index_friendships_on_users_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "stocks", force: :cascade do |t|
